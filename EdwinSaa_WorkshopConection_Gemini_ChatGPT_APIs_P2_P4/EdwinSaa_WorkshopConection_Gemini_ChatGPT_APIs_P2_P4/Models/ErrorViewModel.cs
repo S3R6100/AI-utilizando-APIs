fcf41868a4e6723c71b5f82c612262616127320f
Edwin Saa <edwin.saa@udla.edu.ec>
2025-05-15 07:27:24 -0500

    Add project files.

---
+++ EdwinSaa_WorkshopConection_Gemini_ChatGPT_APIs_P2_P4/EdwinSaa_WorkshopConection_Gemini_ChatGPT_APIs_P2_P4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EdwinSaa_WorkshopConection_Gemini_ChatGPT_APIs_P2_P4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
